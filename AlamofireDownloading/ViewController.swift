d162616ed27f4fa5ad3315e72d9abb489f6d05fb
Abilay10101 <60212530+Abilay10101@users.noreply.github.com>
2022-11-11 17:16:41 +0600

    Used Alamofire to download image

--- AlamofireDownloading/ViewController.swift
+++ AlamofireDownloading/ViewController.swift
@@ -6,14 +6,47 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var completedLabel: UILabel!
+    @IBOutlet weak var indicator: UIActivityIndicatorView!
+    @IBOutlet weak var progressView: UIProgressView!
+    
+    let imageURL = "https://images.theconversation.com/files/378097/original/file-20210111-23-bqsfwl.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=1200.0&fit=crop"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        indicator.startAnimating()
+        getImage(url: imageURL)
     }
 
+    func getImage(url: String) {
+        
+        AF.download(url)
+            
+            .downloadProgress { progress in
+                self.completedLabel.text = progress.localizedDescription
+                self.progressView.setProgress(Float(progress.fractionCompleted), animated: true)
+            }
+            
+            .responseData { response in
+            if let data = response.value {
+                let imagaData = UIImage(data: data)
+                self.imageView.image = imagaData
+                self.indicator.stopAnimating()
+                self.indicator.isHidden = true
+                
+                self.progressView.isHidden = true
+                
+                
+            }
+        }
+    }
 
 }
 
